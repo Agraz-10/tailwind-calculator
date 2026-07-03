300e6c9dd5aeaad3729d635e337929319b20ea58
Agraz248 <185479959+Agraz-10@users.noreply.github.com>
2026-07-03 21:32:04 +0545

    feat: enhance calculator user experience

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 const display = document.getElementById("display");
+const clearButton = document.getElementById("clear");
 const buttons = document.querySelectorAll("button");
 const operators = ["+", "-", "*", "/", "%"];
+
+function updateDisplaySize() {
+    let length = display.value.length;
+
+    display.classList.remove("text-5xl", "text-4xl", "text-3xl", "text-2xl");
+
+    if (length <= 10) {
+        display.classList.add("text-5xl");
+    } else if (length <= 15) {
+        display.classList.add("text-4xl");
+    } else if (length <= 20) {
+        display.classList.add("text-3xl");
+    } else {
+        display.classList.add("text-2xl");
+    }
+}
+
+function updateClearButton()  {
+    
+    if(display.value === "") {
+        clearButton.textContent = "AC";
+    } else {
+        clearButton.textContent = "C";
+    }
+}
 
 buttons.forEach(function(button) {
     button.addEventListener("click", function() {
@@ -8,21 +34,33 @@ buttons.forEach(function(button) {
 
         if (display.value === "Error" && value !== "AC") {
             display.value = "";
-        }
-        
-        if (value === "AC") {
+        } else if (value === "SIGN") {
+            if (display.value !== ""){
+                if (display.value.startsWith("-")) {
+                    display.value = display.value.slice(1);
+                } else {
+                    display.value = "-" + display.value;
+                }
+            } 
+        } else if (value === "AC") {
             display.value = "";
         } else if (value === "DEL") {
             display.value = display.value.slice(0, -1);
         } else if (value === "=") {
             try{
-                display.value = eval(display.value);
+                let result = eval(display.value);
+                
+                if (Number.isInteger(result)) {
+                    display.value = result;
+                } else {
+                    display.value = parseFloat(result.toFixed(10));
+                }
             } catch (error) {
                 display.value = "Error";
             }
         } else {
 
-            if (display.value === "" && ["+", "-", "*", "/", "%"].includes(value)) {
+            if (display.value === "" && ["+", "*", "/", "%"].includes(value)) {
                 return;
             }
 
@@ -40,5 +78,8 @@ buttons.forEach(function(button) {
             }
             display.value += value;
         }
+
+        updateDisplaySize();
+        updateClearButton();
     });
 });
