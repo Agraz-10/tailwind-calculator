d37a11016f7d807fcb754ad8d77b4a669e38a8cc
Agraz248 <185479959+Agraz-10@users.noreply.github.com>
2026-07-03 20:05:42 +0545

    feat: implement calculator operations

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ buttons.forEach(function(button) {
         
         if (value === "AC") {
             display.value = "";
+        }else if (value === "DEL") {
+            display.value = display.value.slice(0, -1);
+        }else if (value === "=") {
+            display.value = eval(display.value);
         }else {
             display.value += value;
         }
